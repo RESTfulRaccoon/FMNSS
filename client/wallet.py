c6d58a5fba3f0d2a5309f50251c48cbd632a8ef3
RESTfulRaccoon <raccoon@rsiot.org>
2024-11-19 19:45:47 -0700

    worked on ssh//oneline bash updater//changed local back to wallet

---
+++ client/wallet.py
@@ -0,0 +1,44 @@
+from subprocess import run,CalledProcessError,PIPE
+from variables import verbose
+
+### CHECK WALLET VERSION
+
+### LOCAL FIROD
+verbose("Starting `firod` on client computer")
+try:
+    result = run(['firod','-daemon'], stdout=PIPE, stderr=PIPE)
+    print('ERRORS: '+str(result.stdout))
+except CalledProcessError as e:
+    ##make this silent unless verbose
+    verbose(e)
+
+### UNLOCK WALLET
+
+### GATHER INFO
+znodeprivkey = "todo!"
+## LOCK WALLET
+
+## Need to figure out why ssh-keygen -lf wont work through paramiko
+
+# result = run(['ssh-keygen','-lf',key,'|','cut','--delimiter','" "','--fields=4'],stdout=PIPE)
+# key_type=result
+# if key_type != '(RSA)' or '(DSS)' or '(ED25519)' or '(EDCSA)':
+#     try:
+#         result = run(['ssh-keygen','-lf',key,'|','cut','--delimiter','" "','--fields=6'],stdout=PIPE)
+#         key_type = result
+#         if key_type != '(RSA)' or '(DSS)' or '(ED25519)' or '(EDCSA)':
+#             print("error")
+#             exit()
+#     except OSError as e:
+#                 print(e)
+# if key_type == '(RSA)':
+#     k = paramiko.RSAKey.from_private_key_file(key)
+# elif key_type == '(DSS)':
+#     k = paramiko.DSSKey.from_private_key_file(key)
+# elif key_type == '(ED25519)':
+#     k=paramiko.Ed25519Key.from_private_key_file(key)
+# elif key_type == '(EDCSA)':
+#     k=paramiko.ECDSAKey.from_private_key_file(key)
+
+
+
